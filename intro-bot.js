4d2195a22bd9f431a8e15645d0bdff49166fb637
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2018-02-18 15:12:27 -0500

    use dotenv for bot key

--- intro-bot.js
+++ intro-bot.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
-const fs = require('fs');
-const client = new Discord.Client();
 const ytdl = require('ytdl-core');
-let voiceConnection;
-let dispatcher;
+const dotenv = require('dotenv');
+const fs = require('fs');
 
-fs.readFileSync('./test.mp3');
+const client = new Discord.Client();
+dotenv.load();
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -14,7 +14,7 @@ client.on('message', msg => {
   if (!msg.guild) return;
 	
 
-  if (msg.content === '/join') {
+  if (msg.content === '!join') {
     // Only try to join the sender's voice channel if they are in one themselves
     if (msg.member.voiceChannel) {
       msg.member.voiceChannel.join()
@@ -27,22 +27,16 @@ client.on('message', msg => {
     }
   }
 
-    if(msg.content === '/play') {
+    if(msg.content === '!play') {
         msg.reply('Playing Test MP3...');
-        //const stream = ytdl('https://youtu.be/9jK-NcRmVcw', { filter: 'audioonly' });
+        const stream = ytdl('https://youtu.be/9jK-NcRmVcw', { filter: 'audioonly' });
         client.voiceConnections.first().playFile('./test.mp3', {volume: 0.5});
-        client.voiceConnections.first().dispatcher.on('error', console.log);
-        client.voiceConnections.first().on('error', console.log);
-        client.voiceConnections.first().on('failed', console.log);
-        client.voiceConnections.first().on('warn', console.log);
-        client.voiceConnections.first().player.on('warn', console.log);
-        client.voiceConnections.first().on('debug', console.log);
     }
 
-    if(msg.content === '/stop') {
+    if(msg.content === '!stop') {
         msg.reply('Stopping playback.');
         client.voiceConnections.first().dispatcher.end();
     }
 });
 
-client.login('NDE0ODMyNzE5MTcyODYxOTYz.DWtGnA.jr-3I8dZ4QSfu40rloSBhc3H6Tk');
+client.login(process.env.DISCORD_BOT_KEY);
